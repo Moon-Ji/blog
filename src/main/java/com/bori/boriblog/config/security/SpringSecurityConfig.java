9d00182b6762c778bd6214928d613762016555a7
moonjiyoon <ske0830@gmail.com>
2022-05-20 20:27:17 +0900

    security 예외 처리

--- src/main/java/com/bori/boriblog/config/security/SpringSecurityConfig.java
+++ src/main/java/com/bori/boriblog/config/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.bori.boriblog.config.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,14 +14,20 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests()
+                    .antMatchers("/chk").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
-                .defaultSuccessUrl("/view/dashboard", true)
-                .permitAll()
+                    .defaultSuccessUrl("/view/dashboard", true)
+                    .permitAll()
                 .and()
-                .logout();
+                    .logout();
 
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/js/**", "/static/css", "/static/img/*", "/static/frontend/**");
     }
 
 }
