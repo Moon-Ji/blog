3dde08f54c79caf51efbbcf2943497c79c5451f1
moonjiyoon <ske0830@gmail.com>
2022-05-20 14:36:01 +0900

    setting

---
+++ src/main/java/com/bori/boriblog/BoriBlogApplication.java
@@ -0,0 +1,13 @@
+package com.bori.boriblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoriBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoriBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bori/boriblog/BoriBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bori.boriblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoriBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
